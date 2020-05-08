7f6a03ed40f5cdb67c7812fd36d71177bcc614c6
Mateus Lima <mateus.lima@osf.digital>
2020-05-08 16:53:12 -0300

    adding implemented routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,82 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const{title} = request.query;
+  //verifica se o titulo do parametro da requisição se inclui no title
+  const results = title
+       ? repositories.filter(repositorie => repositorie.title.includes(title))
+       : repositories;
+
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const newRepository = { id: uuid(), title, url, techs, likes:0};
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository);
+
 });
 
+/*
+  PUT: A rota deve alterar apenas o title, 
+  a url e as techs do repositório que possua o id igual ao id presente nos parâmetros da rota;
+*/
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs, likes} = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found'});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositorieIndex] = repository;
+
+  return response.json(repository);
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found'});
+  }
+  //remove informação contida nesse indice
+  repositories.splice(repositorieIndex, 1);
+  
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found'});
+  }
+
+  
+  const like = ++repositories[repositoryIndex].likes;
+  return response.status(201).json({"likes": like});
+
 });
 
 module.exports = app;
